ae460c37a7a4c426f008698b034b4aab422f314a
motilalsuthar5008 <107179279+motilalsuthar5008@users.noreply.github.com>
2022-10-20 10:18:27 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.efb253c7fa3252c0aefc33f69555361c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d66d4b65af9379681b1",
+    "url": "/textedits/static/css/main.6c086b5b.chunk.css"
+  },
+  {
+    "revision": "5d66d4b65af9379681b1",
+    "url": "/textedits/static/js/main.5d66d4b6.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/textedits/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/textedits/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "6bc4ad0f3b7955693c4b",
+    "url": "/textedits/static/js/runtime~main.6bc4ad0f.js"
+  },
+  {
+    "revision": "ca733424e78e60304581f3ca7acc26cf",
+    "url": "/textedits/index.html"
+  }
+];
